ed1ff6d81d87e9593cbcd49efb33a1edcd6e555d
Saul Enriquez <166153844+saulEnriquezpioquinto@users.noreply.github.com>
2026-06-17 19:56:31 -0600

    Add age input and evaluation for adulthood

--- ejer2jo.py
+++ ejer2jo.py
@@ -1,5 +1,9 @@
+
+#le pide al usuario que ingrese su edad 
 edad = int(input("ingresa tu edad: "))
+#evalua si es mayor o igual a 18 
 if edad >= 18:
     print("Eres mayor de edad")
+    #si no es mayor o igual a 18 
 else:
     print("Eres menor de edad")
